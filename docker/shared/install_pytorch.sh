494dd8a1b1bf5543373ea6eb5fe2e27745b29a6a
RSPwFPGAs <RSPwFPGAs@github.com>
2020-09-19 07:06:36 +0800

    Added script to export ONNX model.

--- docker/shared/install_pytorch.sh
+++ docker/shared/install_pytorch.sh
@@ -1,4 +1,6 @@
 apt update
 apt install sudo
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip3 install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+pip3 install netron
